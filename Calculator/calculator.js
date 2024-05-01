a8f7c7705f74f012816bf482400ae8ec3b992696
shreya <shreyamrao@gmail.com>
2024-05-01 11:55:03 +0530

    calc added delete functionality

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -2,13 +2,13 @@ let calculation = localStorage.getItem('calculation') || '';
                 
 function updateCalculation(addToCalculation){            
     calculation += addToCalculation;
+    document.querySelector('.js-equation').innerHTML = calculation    
     localStorage.setItem('calculation', calculation)
-    document.querySelector('.js-equation').innerHTML = calculation
 }
 
 function clearEquation(){
     calculation =''; 
-    document.querySelector('.js-equation').innerHTML = calculation 
+    document.querySelector('.js-equation').innerHTML = 'calculation' 
     localStorage.setItem('calculation', calculation);
 }
 
@@ -16,4 +16,10 @@ function calculations(){
     calculation=eval(calculation); 
     document.querySelector('.js-equation').innerHTML = calculation
     localStorage.setItem('calculation', calculation);
+}
+
+function deleteChar(){
+    calculation = calculation.slice(0, -1)
+    calculation === '' ? document.querySelector('.js-equation').innerHTML = 'calculation' : document.querySelector('.js-equation').innerHTML = calculation;
+    localStorage.setItem('calculation', calculation)
 }
